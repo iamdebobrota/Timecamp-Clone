eeb26004e70a63cf3a8dec9fbed62d4543386b00
Debobrota Haldar <99647439+iamdebobrota@users.noreply.github.com>
2022-07-19 20:19:38 +0530

    features page init
    features page init

---
+++ frontend/src/components/pages/Features/Features.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './features.module.css'
+
+
+const Features = () => {
+  return (
+    <div className={style.style}>
+         
+    </div>
+  )
+}
+
+export default Features
