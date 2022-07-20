4c2fc5603349d08f8d3678560a699a9ca804e733
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-07-20 15:17:52 +0530

    Update ClientModel.js

--- backend/models/ClientModel.js
+++ backend/models/ClientModel.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-const ClientSchema = mongoose.Schema({
-    
+const clientSchema = mongoose.Schema({
+    email: {type: String, required:true},
+    password : {type: String, required:true},
+    phone:{type: Number, required:false}
 })
+
+const ClientModel = mongoose.Model("client", clientSchema)
+
+module.exports = ClientModel ;
