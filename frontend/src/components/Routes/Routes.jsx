b93d2f18ceca4eca8c04cb9cb2dc16f9f8d13177
Aditya Sekhar Bej <95712637+adityasekharbej@users.noreply.github.com>
2022-07-21 01:14:12 +0530

    header updated

--- frontend/src/components/Routes/Routes.jsx
+++ frontend/src/components/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Features from '../pages/Features/Features'
 import Footer from '../pages/Footer/Footer'
+import { Header } from '../pages/Header/Header'
 
 const Routes = () => {
   return (
     <div>
         
+        <Header/>
         <Features/>
         <Footer/>
     </div>
