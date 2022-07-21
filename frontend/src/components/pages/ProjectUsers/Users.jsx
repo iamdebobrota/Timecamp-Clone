d21030a070051c05ada2c29910d999a675481440
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-07-22 02:36:42 +0530

    alertDiv

--- frontend/src/components/pages/ProjectUsers/Users.jsx
+++ frontend/src/components/pages/ProjectUsers/Users.jsx
@@ -1,19 +1,24 @@
 import React from "react";
+import styles from "../ProjectUsers/users.module.css"
 
 const Users = () => {
   return (
     <div>
       <div>
-        
+
       </div>
-      <div  className="alert alert-info ng-binding" id="users-alert" role="alert">
-      <i className="fa fa-user-plus" aria-hidden="true"></i>
+      <div  className={styles.usersAlertDiv} id="users-alert" role="alert">
+      <i style={{fontSize:'26px', cursor: 'pointer'}} className="fa fa-user-plus" aria-hidden="true"></i>
+      
         You can now invite guests like clients, vendors, or contractors. You can
-        add them free of charge.
+        add them free of charge. {" "}
         <a className="track-click ng-scope">
-          <b className="ng-binding">Invite guest</b>
+          <b style={{cursor: 'pointer', color:"#6393f8", marginLeft:'-40px'}} className="ng-binding">Invite guest</b>
         </a>
       </div>
+
+
+      
     </div>
   );
 };
