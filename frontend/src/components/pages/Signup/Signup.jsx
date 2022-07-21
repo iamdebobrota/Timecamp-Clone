b0e6d9cce3b1cabb725688496524c64314cd4620
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-07-21 15:57:11 +0530

    signup component, css and implementation with backend done

--- frontend/src/components/pages/Signup/Signup.jsx
+++ frontend/src/components/pages/Signup/Signup.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useState } from 'react'
+import {useNavigate} from "react-router-dom"
 import styles from "./signup.module.css"
 
 const Signup = () => {
 
     const [client, setClient] = useState({});
+    const  navigate= useNavigate()
 
     const handleChange=(e)=>{
         let {name , value } = e.target
@@ -26,7 +28,8 @@ const Signup = () => {
             body: payload
         })
         .then((res)=> res.json())
-        .then((res) => )
+        .then((res) => navigate("/login"))
+        .catch((err)=> console.log(err))
         
     }
 
@@ -104,32 +107,3 @@ const Signup = () => {
 
 export default Signup
 
-//  {/* <div className="register-wrapper__text-column col-sm-12 col-md-7 col-lg-7 ">
-//         <div className="register-wrapper__headingText hidden-xs hidden-sm">
-//         <h1>Loved by customers and recognized among online communities. </h1>
-//         </div>
-//         <div className="register-wrapper__img--review">
-//         <img src="https://cdn.timecamp.com/res/css/images/review-icon-signup.1658227855.svg" />
-//         </div>
-//         <div className="register-wrapper__reviews-columns">
-//         <div className="register-wrapper__reviews-columns--single">
-//         <p>Counting every second is easy with TimeCamp. </p>
-//         <img src="https://cdn.timecamp.com/res/css/images/stars-reviews-signup.1658227855.svg" />
-//         <p><span>Emily, </span>Lead Software Engineer </p>
-//         </div>
-//         <div className="register-wrapper__reviews-columns--single">
-//         <p>Great tool to measure the time dedicated to tasks and projects. </p>
-//         <img src="https://cdn.timecamp.com/res/css/images/stars-reviews-signup.1658227855.svg" />
-//         <p><span>Enrique, </span>Director Operaciones at Consultor </p>
-//         </div>
-//         <div className="register-wrapper__reviews-columns--single">
-//         <p>It has removed all our worries about tracking time and activities. </p>
-//         <img src="https://cdn.timecamp.com/res/css/images/stars-reviews-signup.1658227855.svg" />
-//         <p><span>Sarah, </span>Practice Lead at Cloud &amp; Emerging Tech </p>
-//         </div>
-//         </div>
-//         <div className="register-wrapper__img--icons">
-//         <img src="https://cdn.timecamp.com/res/css/images/leader-summer-reviews-signup.1658227855.svg" />
-//         <img src="https://cdn.timecamp.com/res/css/images/easiest-to-use-icon-review-signup.1658227855.svg" />
-//         </div>
-//         </div> */}
