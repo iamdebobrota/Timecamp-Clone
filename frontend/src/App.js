1e2a42960e0b799c6ad950d8eb4121df3b13f54a
Aditya Sekhar Bej <95712637+adityasekharbej@users.noreply.github.com>
2022-07-23 13:38:01 +0530

    dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { Dashboard } from './components/pages/Dashboard/Dashboard';
 import SideNavbar from './components/pages/SideNav/SideNavbar';
 import Router from './components/Routes/Router';
 function App() {
   return (
     <div className="App">
-      <Router/>
-
+      {/* <Router/> */}
+      <Dashboard/>
       {/* <SideNavbar/> */}
   </div>
   );
