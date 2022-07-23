233afcb347ea942b0afb661bfe7b299d79079bd0
Aditya Sekhar Bej <95712637+adityasekharbej@users.noreply.github.com>
2022-07-24 00:23:44 +0530

    project page update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import { Project } from './components/pages/ProjectPage/Project';
 import SideNavbar from './components/pages/SideNav/SideNavbar';
 import Router from './components/Routes/Router';
 function App() {
   return (
     <div className="App">
-      <Router/>
-
+      {/* <Router/> */}
+      <Project/>
       {/* <SideNavbar/> */}
   </div>
   );
