573ece189ef34c1559b49bf8650ec575fb83000c
Anurag Verma <anurag.premaverma@gmail.com>
2022-07-24 02:36:00 +0530

    Tags added

--- frontend/src/components/pages/Projectpage/Tags.jsx
+++ frontend/src/components/pages/Projectpage/Tags.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
+import { useState } from 'react'
 import TimeHeader from '../TimeHeader/TimeHeader'
 import style from './project.module.css'
 
 const Tags = () => {
+  const [show, setShow]= useState(false)
   return (
+    <div className={style.contatiner}>
     <div className={style.tags_main}>
         <TimeHeader heading={'Tags'}/>
             <div className={style.tags}>
-                Tags
+              <button className={style.tagbtn}>+Add tag list</button>
+                {/* Tags */}
+                <p className={style.tagpara}>Manage your tags, like list of customers or activities. All workspace members can assign tags to time entries, when they track time. Project managers can set lists of tags for projects. Reports can be filtered and grouped by tags.</p>
                 
+                <button className={style.showhidebtn}>{show?"Show archived tags":"Hide archived tags"}</button>
+
+                <  hr className={style.hrtag}/>
+
+                <div className="dividerbtn">
+                  <button className={style.savebtn}>Save Setting</button>
+                  <button className={style.backbtn}>Back Addons list</button>
+                </div>
             </div>
+
+
+    </div>
     </div>
   )
 }
